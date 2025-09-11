3359dfb5f0d9a8b43a90db2d28182ecc43ee5634
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2025-09-11 20:04:15 +0600

    user controller & user service done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,6 +6,7 @@ import { catchAsync } from "../../utils/catchAsync";
 // import { sendResponse } from "../../utils/sendResponse";
 import { UserServices } from "./user.service";
 import { sendResponse } from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -20,9 +21,32 @@ const createUser = catchAsync(
   }
 );
 
+const updateUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
+    const verifiedToken = req.user;
+    const payload = req.body;
+    const user = await UserServices.updateUser(
+      userId,
+      payload,
+      verifiedToken as JwtPayload
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Updated Successfully",
+      data: user,
+    });
+  }
+);
+
 const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const result = await UserServices.getAllUsers();
+    const query = req.query;
+    const result = await UserServices.getAllUsers(
+      query as Record<string, string>
+    );
 
     sendResponse(res, {
       success: true,
@@ -34,9 +58,38 @@ const getAllUsers = catchAsync(
   }
 );
 
+const getMe = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user as JwtPayload;
+    const result = await UserServices.getMe(decodedToken.userId);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "Your profile Retrieved Successfully",
+      data: result.data,
+    });
+  }
+);
+
+const getSingleUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const result = await UserServices.getSingleUser(id);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Retrieved Successfully",
+      data: result.data,
+    });
+  }
+);
+
 export const UserControllers = {
   createUser,
   getAllUsers,
+  getSingleUser,
+  updateUser,
+  getMe,
 };
-
 // route matching -> controller -> service -> model -> DB
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,19 +1,24 @@
-import { Router } from "express"
-import { UserRoutes } from "../modules/user/user.route"
+import { Router } from "express";
+import { UserRoutes } from "../modules/user/user.route";
+import { AuthRoutes } from "../modules/auth/auth.route";
 
-export const router = Router()
+export const router = Router();
 
 const moduleRoutes = [
-    {
-        path: "/user",
-        route: UserRoutes
-    },
-    // {
-    //     path: "/tour",
-    //     route: TourRoutes
-    // },
-]
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  // {
+  //     path: "/tour",
+  //     route: TourRoutes
+  // },
+];
 
 moduleRoutes.forEach((route) => {
-    router.use(route.path, route.route)
-})
+  router.use(route.path, route.route);
+});
