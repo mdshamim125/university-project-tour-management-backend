763ef10429edc34765a0e2bc59daa83c955469a1
unknown <cse12005038brur@gmail.com>
2025-10-15 15:49:29 +0600

    added strategy for authentication

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,43 @@
-import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import { router } from "./app/routes";
+import express, { Request, Response } from "express";
+import expressSession from "express-session";
+import passport from "passport";
+import { envVars } from "./app/config/env";
+import "./app/config/passport";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
 
 const app = express();
 
+app.use(
+  expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
-app.use("api/v1", router);
+app.use("/api/v1", router);
 
-app.get("/", (req: Request, res: Response) => [
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to the Tour Management System",
-  }),
-]);
+    message: "Welcome to Tour Management System Backend",
+  });
+});
 
 app.use(globalErrorHandler);
 
