a4c9196385978e83305615e020c4b34e74584c5d
unknown <cse12005038brur@gmail.com>
2025-10-21 00:11:55 +0600

    some modification

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -26,77 +26,6 @@ const createTour = async (payload: ITour) => {
     return tour;
 };
 
-// const getAllToursOld = async (query: Record<string, string>) => {
-//     console.log(query);
-//     const filter = query
-//     const searchTerm = query.searchTerm || "";
-//     const sort = query.sort || "-createdAt";
-//     const page = Number(query.page) || 1
-//     const limit = Number(query.limit) || 10
-//     const skip = (page - 1) * limit
-
-//     //field fitlering
-//     const fields = query.fields?.split(",").join(" ") || ""
-
-//     //old field => title,location
-//     //new fields => title location
-
-//     // delete filter["searchTerm"]
-//     // delete filter["sort"]
-
-
-
-//     for (const field of excludeField) {
-//         // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-//         delete filter[field]
-//     }
-
-//     console.log(filter);
-
-
-
-//     const searchQuery = {
-//         $or: tourSearchableFields.map(field => ({ [field]: { $regex: searchTerm, $options: "i" } }))
-//     }
-
-//     // [remove][remove][remove](SKip)[][][][][][]
-
-//     // [][][][][](limit)[remove][remove][remove][remove]
-
-//     // 1 page => [1][1][1][1][1][1][1][1][1][1] skip = 0 limit =10
-//     // 2 page => [1][1][1][1][1][1][1][1][1][1]=>skip=>[2][2][2][2][2][2][2][2][2][2]<=limit skip = 10 limit =10
-//     // 3 page => [1][1][1][1][1][1][1][1][1][1]=>skip=>[2][2][2][2][2][2][2][2][2][2]<=limit skip = 20 limit = 10
-
-//     // skip = (page -1) * 10 = 30
-
-//     // ?page=3&limit=10
-
-//     // const tours = await Tour.find(searchQuery).find(filter).sort(sort).select(fields).skip(skip).limit(limit);
-
-//     const filterQuery = Tour.find(filter)
-
-//     const tours = filterQuery.find(searchQuery)
-
-//     const allTours = await tours.sort(sort).select(fields).skip(skip).limit(limit)
-
-//     // location = Dhaka
-//     // search = Golf
-//     const totalTours = await Tour.countDocuments();
-//     // const totalPage = 21/10 = 2.1 => ciel(2.1) => 3
-//     const totalPage = Math.ceil(totalTours / limit)
-
-//     const meta = {
-//         page: page,
-//         limit: limit,
-//         total: totalTours,
-//         totalPage: totalPage,
-//     }
-//     return {
-//         data: allTours,
-//         meta: meta
-//     }
-// };
-
 const getAllTours = async (query: Record<string, string>) => {
 
 
@@ -136,17 +65,7 @@ const updateTour = async (id: string, payload: Partial<ITour>) => {
         throw new Error("Tour not found.");
     }
 
-    // if (payload.title) {
-    //     const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-    //     let slug = `${baseSlug}`
 
-    //     let counter = 0;
-    //     while (await Tour.exists({ slug })) {
-    //         slug = `${slug}-${counter++}` // dhaka-division-2
-    //     }
-
-    //     payload.slug = slug
-    // }
 
     if (payload.images && payload.images.length > 0 && existingTour.images && existingTour.images.length > 0) {
         payload.images = [...payload.images, ...existingTour.images]
@@ -177,13 +96,13 @@ const deleteTour = async (id: string) => {
     return await Tour.findByIdAndDelete(id);
 };
 const createTourType = async (payload: ITourType) => {
-    const existingTourType = await TourType.findOne({ name: payload.name });
+    const existingTourType = await TourType.findOne({ name: payload });
 
     if (existingTourType) {
         throw new Error("Tour type already exists.");
     }
 
-    return await TourType.create({ name });
+    return await TourType.create({ name: payload });
 };
 const getAllTourTypes = async (query: Record<string, string>) => {
     const queryBuilder = new QueryBuilder(TourType.find(), query)
