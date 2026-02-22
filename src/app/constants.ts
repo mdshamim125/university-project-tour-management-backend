bdcb3f8d84322f84b53312a8343b13d8feed4c13
unknown <cse12005038brur@gmail.com>
2026-02-22 14:20:44 +0600

    modified the version of cloudinary

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,26 @@
 export const excludeField = ["searchTerm", "sort", "fields", "page", "limit"];
+
+
+//   "dependencies": {
+//     "axios": "^1.12.2",
+//     "bcryptjs": "^3.0.2",
+//     "cloudinary": "^2.7.0",
+//     "cookie-parser": "^1.4.7",
+//     "cors": "^2.8.5",
+//     "dotenv": "^17.2.1",
+//     "ejs": "^3.1.10",
+//     "express": "^5.1.0",
+//     "express-session": "^1.18.2",
+//     "http-status-codes": "^2.3.0",
+//     "jsonwebtoken": "^9.0.2",
+//     "mongoose": "^8.17.1",
+//     "multer": "^2.0.2",
+//     "multer-storage-cloudinary": "^4.0.0",
+//     "nodemailer": "^7.0.6",
+//     "passport": "^0.7.0",
+//     "passport-google-oauth20": "^2.0.0",
+//     "passport-local": "^1.0.0",
+//     "pdfkit": "^0.17.2",
+//     "redis": "^5.8.2",
+//     "zod": "^4.0.17"
+//   }
